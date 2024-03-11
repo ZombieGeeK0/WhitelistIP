fe0db863e70b10bfd1a4f82b36789dbb778c46f6
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-03-11 14:44:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-clear && sudo apt update && sudo apt upgrade && sudo apt install python3 && sudo apt install python3-pip && pip3 install requests && pip3 install time && pip3 install sys && pip3 install socket
+clear && sudo apt update && sudo apt upgrade && sudo apt install python3 && sudo apt install python3-pip && pip3 install requests && pip3 install sys && pip3 install socket
